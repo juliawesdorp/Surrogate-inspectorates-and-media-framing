7e2bc34e9d46f82bd445b9fe3dfd44a7d709adcb
juliawesdorp <wesdorp@essb.eur.nl>
2024-12-03 15:28:54 +0100

    R_Script.R

--- Full_R_Script.R
+++ Full_R_Script.R
@@ -1,16 +1,7 @@
 # ==== 1. INTRODUCTION =====
 rm(list = ls())
 
-# Numbers and the regulatory agencies
-## 1 = NVWA
-## 2 = ILT
-## 3 = ACM
-## 4 = SodM
-## 5 = DCMR
-## 8 = AP
-## Number 6 and 7 were not used in this study.
-
-# Used packages (install them when necessary)
+# Used packages
 library(dplyr)
 library(lubridate)
 library(ggplot2)
@@ -27,42 +18,34 @@ dataset <- dataset %>%
                           Q12_Onderzoeksinstituut, Q12_Media, Q12_BNer, Q12_Stichting, Q12_Belangenvereniging, 
                           Q12_Overig, na.rm = TRUE))
 
-table(dataset$surrogate)
-
 dataset["Q9_Conflict"][dataset["Q9_Conflict"] == 1] <- 0
 dataset["Q9_Conflict"][dataset["Q9_Conflict"] == 2] <- 1
-
 dataset["Q10_Sensatie"][dataset["Q10_Sensatie"] == 1] <- 0
 dataset["Q10_Sensatie"][dataset["Q10_Sensatie"] == 2] <- 1
+dataset$binary_personalization <- ifelse(dataset$Q8_Person>1,1,0)
+dataset$binary_toon <- ifelse(dataset$Q7_Toon>1,0,1)
 
 # ==== 3. ANALYSES =====
 # ==== 3.1 Binary logistic regression - creating models=====
 # Creating the models
 # Conflict
-table(dataset$Q9_Conflict)
 log_model_conflict_control <- glm(Q9_Conflict ~ Q3_Inspectie + Q1_Medium, data = dataset, family = binomial())
 log_model_conflict <- glm(Q9_Conflict ~ surrogate + Q3_Inspectie + Q1_Medium, data = dataset, family = binomial())
 summary(log_model_conflict)
 summary(log_model_conflict_control)
 
 # Sensation
-table(dataset$Q10_Sensatie)
-
 log_model_sensatie <- glm(Q10_Sensatie ~ surrogate + Q3_Inspectie + Q1_Medium, data = dataset, family = binomial())
 log_model_sensatie_control <- glm(Q10_Sensatie ~ Q3_Inspectie + Q1_Medium, data = dataset, family = binomial())
 summary(log_model_sensatie)
 
 # Personalization
-table(dataset$binary_personalization)
-
 log_model_pers <- glm(binary_personalization ~ surrogate + Q3_Inspectie + Q1_Medium, data = dataset, family = binomial())
 log_model_pers_control <- glm(binary_personalization ~ Q3_Inspectie + Q1_Medium, data = dataset, family = binomial())
 summary(log_model_pers)
 summary(log_model_pers_control)
 
 # Valence
-table(dataset$binary_toon)
-table(dataset$Q7_Toon)
 log_model_toon <- glm(binary_toon ~ surrogate + Q3_Inspectie + Q1_Medium, data = dataset, family = binomial())
 log_model_toon_control <- glm(binary_toon ~ Q3_Inspectie + Q1_Medium, data = dataset, family = binomial()) 
 summary(log_model_toon)
@@ -72,11 +55,9 @@ summary(log_model_toon)
 ## Calculate Chi squared
 summary(log_model_conflict)
 modelChi_conflict <- log_model_conflict$null.deviance - log_model_conflict$deviance
-modelChi_conflict
 chidf_conflict <- log_model_conflict$df.null - log_model_conflict$df.residual
-chidf_conflict
 chisq.prob_conflict <- 1 - pchisq(modelChi_conflict, chidf_conflict)
-chisq.prob_conflict
+
 
 ## Confidence interval and odds ratio
 exp(log_model_conflict$coefficients)
@@ -86,11 +67,8 @@ exp(confint(log_model_conflict))
 ## Calculate Chi squared
 summary(log_model_pers)
 modelChi_p <- log_model_pers$null.deviance - log_model_pers$deviance
-modelChi_p
 chidf_p <- log_model_pers$df.null - log_model_pers$df.residual
-chidf_p
 chisq.prob_p <- 1 - pchisq(modelChi_p, chidf_p)
-chisq.prob_p
 
 ## Confidence interval and odds ratio
 exp(log_model_pers$coefficients)
@@ -100,9 +78,7 @@ exp(confint(log_model_pers))
 ## Calculate Chi squared
 summary(log_model_sensatie)
 modelChi_s <- log_model_sensatie$null.deviance - log_model_sensatie$deviance
-modelChi_s
 chidf_s <- log_model_sensatie$df.null - log_model_sensatie$df.residual
-chidf_s
 chisq.prob_s <- 1 - pchisq(modelChi_s, chidf_s)
 chisq.prob_s
 
@@ -114,19 +90,13 @@ exp(confint(log_model_sensatie))
 model_difference_sensatie <- log_model_sensatie_control$deviance - log_model_sensatie$deviance
 chidf_difference_sensatie <- log_model_sensatie_control$df.residual - log_model_sensatie$df.residual
 chisq.prob_difference_s <- 1 - pchisq(model_difference_sensatie, chidf_difference_sensatie)
-model_difference_sensatie
-chidf_difference_sensatie
-chisq.prob_difference_s
 
 # VALENCE
 ## Calculate Chi squared
 summary(log_model_toon)
 modelChi_t <- log_model_toon$null.deviance - log_model_toon$deviance
-modelChi_t
 chidf_t <- log_model_toon$df.null - log_model_toon$df.residual
-chidf_t
 chisq.prob_t <- 1 - pchisq(modelChi_t, chidf_t)
-chisq.prob_t
 
 ## Confidence interval and odds ratio
 exp(log_model_toon$coefficients)
@@ -136,9 +106,6 @@ exp(confint(log_model_toon))
 model_difference_toon <- log_model_toon_control$deviance - log_model_toon$deviance
 chidf_difference_toon <- log_model_toon_control$df.residual - log_model_toon$df.residual
 chisq.prob_difference_t <- 1 - pchisq(model_difference_toon, chidf_difference_toon)
-model_difference_toon
-chidf_difference_toon
-chisq.prob_difference_t
 
 # ==== 3.3 Binary logistic regression - R2 =====
 ## McFadden
@@ -176,9 +143,8 @@ names_regulator <- c(
   "6" = "AP")
 
 colors <- c("red2", "royalblue1", "forestgreen", "orange", "deeppink1", "darkmagenta")
-articles_per_year$Q3_Inspectie <- factor(articles_per_year$Q3_Inspectie)
 
-time_regulators <- ggplot(articles_per_year, aes(x=year, y=article_count, color = Q3_Inspectie)) +
+articles_per_regulator <- ggplot(articles_per_year, aes(x=year, y=article_count, color = Q3_Inspectie)) +
   geom_line() + 
   geom_point() +
   geom_smooth(method = "lm", col = "blue")+
@@ -186,18 +152,15 @@ time_regulators <- ggplot(articles_per_year, aes(x=year, y=article_count, color 
   facet_wrap(~Q3_Inspectie, scales = "free_y", labeller = labeller(Q3_Inspectie = names_regulator))+
   scale_x_continuous(breaks = pretty(dataset$year, n = 8)) +
   scale_color_manual(values = colors)
-time_regulators
 
 ## Graph - Valence over the years
 valence_per_year <- dataset %>%
   group_by(year, Q7_Toon) %>%
   summarise(article_count = n())
-valence_per_year
 
 filtered_data <- valence_per_year %>%
   filter(Q7_Toon == "1")
-filtered_data
-table(dataset$year)
+
 valence_years <- ggplot(filtered_data, aes(x=year, y=article_count)) +
   geom_line() +
   geom_smooth(method = "lm", col = "blue")+
@@ -209,8 +172,6 @@ valence_years <- ggplot(filtered_data, aes(x=year, y=article_count)) +
     axis.title.y = element_text(face="bold", size = 14),
     axis.text = element_text(face="bold", size = 12)
   )
-
-valence_years
 
 ## Graph - Percentages negativity  - because each year has different sample sizes
 df_n <- data.frame(
@@ -229,5 +190,3 @@ negativity_controlled <- ggplot(df_n, aes(x=Year, y = Percentage, group = 1)) +
     axis.title.y = element_text(face="bold", size = 14),
     axis.text = element_text(face="bold", size = 12)
   )
-
-negativity_controlled
